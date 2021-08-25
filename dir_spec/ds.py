f29b64701e672278eba9c637fae82d8d9f099413
Neil Gallagher <neil.gallagher@duke.edu>
2021-08-24 22:33:24 -0400

    tested: runs without errors

--- dir_spec/ds.py
+++ dir_spec/ds.py
@@ -11,10 +11,10 @@ ds : Return a DirectedSpectrum object for multi-channel timeseries data.
 from itertools import combinations
 import numpy as np
 from scipy.signal import csd
-from scipy.fft import ifft
+from scipy.fft import fft, ifft
 from numpy.linalg import cholesky, solve
 
-# for debugging purposes
+# for debugging purposes !!!
 import matplotlib.pyplot as plt
 
 
@@ -23,22 +23,19 @@ class DirectedSpectrum(object):
 
     Attributes
     ----------
-    ds_array : ndarray, shape (n_groups, n_groups, n_frequencies)
+    ds_array : ndarray, shape (n_frequencies, n_groups, n_groups)
         Directed spectrum values between each pair of channel
         groups for each frequency.
     f : ndarray, shape (n_frequencies)
         Frequencies associated with the last dimension of ds_array.
-    sources : ndarray of strings, shape (n_groups, n_groups)
-        Names the source channel groups for each value in ds_array.
-    targets : ndarray of strings, shape (n_groups, n_groups)
-        Names the target channel groups for each value in ds_array.
+    cgroups : ndarray of strings, shape (n_groups)
+        Names the channel groups associated with ds_array.
     """
 
-    def __init__(self, ds_array, f, sources, targets):
+    def __init__(self, ds_array, f, cgroups):
         self.ds_array = ds_array
         self.f = f
-        self.sources = sources
-        self.targets = targets
+        self.cgroups = cgroups
 
 def ds(X, fs, groups, pairwise=False, fres=None, window='hann', nperseg=None,
        noverlap=None):
@@ -93,36 +90,33 @@ def ds(X, fs, groups, pairwise=False, fres=None, window='hann', nperseg=None,
     dir_spec : DirectedSpectrum object
     """
     cpsd, f = _cpsd_mat(X, fs, fres, window, nperseg, noverlap)
+    # move frequency to first dimension
+    cpsd = np.moveaxis(cpsd, 2, 0)
 
     gidx, grouplist = _group_indicies(groups)
     group_pairs = combinations(range(len(gidx)), 2)
-    ds_array = np.zeros((len(gidx), len(gidx), len(f)))
+    ds_array = np.zeros((len(f), len(gidx), len(gidx)))
 
     if not pairwise:
         H, Sigma = _wilson_factorize(cpsd, fs)
     for gp in group_pairs:
-        idx0 = gidx[gp[0]]
-        idx1 = gidx[gp[1]]
-        pair_idx = np.hstack((idx0, idx1))
-        sub_idx1 = np.hstack((np.zeros(len(idx0)), np.ones(len(idx1))))
-        import pdb; pdb.set_trace()
+        idx0 = np.array(gidx[gp[0]])
+        idx1 = np.array(gidx[gp[1]])
+        pair_idx = np.nonzero(idx0 | idx1)[0]
+        sub_idx1 = idx1[pair_idx]
         if pairwise:
-            sub_cpsd = cpsd[pair_idx, pair_idx]
+            sub_cpsd = cpsd.take(pair_idx, axis=1).take(pair_idx, axis=2)
             H, Sigma = _wilson_factorize(sub_cpsd, fs)
             ds01, ds10 = _var_to_ds(H, Sigma, sub_idx1)
         else:
-            sub_H = H[pair_idx, pair_idx]
-            sub_Sigma = Sigma[pair_idx, pair_idx]
+            sub_H = H.take(pair_idx, axis=1).take(pair_idx, axis=2)
+            sub_Sigma = Sigma.take(pair_idx, axis=0).take(pair_idx, axis=1)
             ds01, ds10 = _var_to_ds(sub_H, sub_Sigma, sub_idx1)
         # sum across channels within group
-        ds_array[gp[0], gp[1]] = np.sum(ds01)
-        ds_array[gp[1], gp[0]] = np.sum(ds10)
+        ds_array[:, gp[0], gp[1]] = np.linalg.det(ds01)
+        ds_array[:, gp[1], gp[0]] = np.linalg.det(ds10)
 
-    import pdb; pdb.set_trace()
-    sources = np.tile(grouplist, (1, len(grouplist)))
-    targets = np.tile(grouplist.T, (len(grouplist), 1))
-
-    return DirectedSpectrum(ds_array, f, directed_pairs, sources, targets)
+    return DirectedSpectrum(ds_array, f, grouplist)
 
 def _cpsd_mat(X, fs, fres, window, nperseg, noverlap):
     """Return cross power spectral density and associated frequencies."""
@@ -138,7 +132,7 @@ def _cpsd_mat(X, fs, fres, window, nperseg, noverlap):
     for c1 in range(C):
         for c2 in range(c1, C):
             f, this_csd = csd(X[c1], X[c2], fs, window, nperseg, noverlap,
-                              nfft, return_onesided=False)
+                              nfft, return_onesided=False, scaling='density')
             cpsd[c1, c2] = this_csd
             cpsd[c2, c1] = this_csd.T.conj()
     return (cpsd, f)
@@ -149,7 +143,7 @@ def _group_indicies(groups):
     gidx = [[g1==g2 for g1 in groups] for g2 in grouplist]
     return (gidx, grouplist)
 
-def _wilson_factorize(cpsd, fs, max_iter=500, tol=1e-9):
+def _wilson_factorize(cpsd, fs, max_iter=1000, tol=1e-9):
     """Factorize CPSD into transfer matrix (H) and covariance (Sigma)
 
     Implements the algorithm outlined in the following reference:
@@ -159,68 +153,88 @@ def _wilson_factorize(cpsd, fs, max_iter=500, tol=1e-9):
     """
     psi = _init_psi(cpsd)
 
-    # move frequency to first dimension
-    psi = np.moveaxis(psi, 2, 0)
-    cpsd = np.moveaxis(cpsd, 2, 0)
-
+    # add small number to cpsd to prevent it from being negative
+    # semidefinite due to rounding errors
+    this_eps = np.spacing(np.abs(cpsd)).max()
+    U = cholesky(cpsd + np.identity(cpsd.shape[-1])*2*this_eps)
+    from time import time
+    start = time()
     for k in range(max_iter):
         # g = psi \ cpsd / psi* + I
-        import pdb; pdb.set_trace()
-        psi_inv_cpsd = solve(psi, cpsd)
-        g = solve(psi.conj(), psi_inv_cpsd.transpose(0,2,1)).transpose(0,2,1)
+        psi_inv_cpsd = solve(psi, U)
+        g = psi_inv_cpsd @ psi_inv_cpsd.conj().transpose(0, 2, 1)
         g = g + np.identity(cpsd.shape[-1])
 
-        gplus, gp0 = _plus_operator(g)
-        S = -np.tril(gp0, -1)
-        S = S - S.H
-        gplus = gplus + S # check for upper triang psi0
+        gplus = _plus_operator(g)
+        S = -np.tril(gplus[0], -1)
+        S = S - S.conj().transpose()
+        gplus = gplus + S
         psi_prev = psi
         psi = psi @ gplus
 
-        if _check_convergence(psi, psi_prev, tol):
+        converged = _check_convergence(psi, psi_prev, tol)
+        if converged:
+            print(f'converged! - {k:d} iters')
             break
+    print(f'{time()-start:.3f}s elapsed')
 
-        A = real(ifft(psi)) # real?!?
-        Sigma = A[0] @ A[0].T # fs scaling?!?
+    A = ifft(psi, axis=0).real
+    Sigma = A[0] @ A[0].T # fs scaling?!?
+    H = (solve(A[0].T, psi.transpose(0, 2, 1))).transpose(0, 2, 1) # right-side solve
+    return (H, Sigma)
 
-        H = (solve(A[0].T, psi.T)).T
-
-        return (H, Sigma)
 
 def _init_psi(cpsd):
     """Return initial psi value for wilson factorization."""
     # try other init?
-    gamma = ifft(cpsd) # set workers for parallelization?
-    gamma0 = np.asmatrix(gamma[...,0])
+    gamma = ifft(cpsd, axis=0) # set workers for parallelization?
+    gamma0 = np.asmatrix(gamma[0])
     # remove assymetry due to rounding error
     gamma0 = np.real((gamma0+gamma0.H)/2)
-    h = np.asarray(cholesky(gamma0))
-    psi = np.tile(h[...,np.newaxis], (1, 1, gamma.shape[-1]))
+    h = np.asarray(cholesky(gamma0).H)
+    psi = np.tile(h[np.newaxis], (gamma.shape[0], 1, 1))
     return psi
+
+def _plus_operator(g):
+    """Remove all negative lag components from time-domain representation"""
+    # remove imaginary components due to rounding error
+    gamma = ifft(g, axis=0).real
+
+    # take half of 0 lag
+    gamma[0] *= 0.5
+
+    # take half of nyquist component if fft had even # of points
+    F = gamma.shape[0]
+    N = int(np.floor(F/2))
+    if F % 2 == 0:
+        gamma[N] *= 0.5
+
+    # zero out negative frequencies
+    gamma[N+1:] = 0
+
+    gp = fft(gamma, axis=0)
+    return gp
 
 def _check_convergence(x, x0, tol):
     """Determine whether maximum change is lower than tolerance."""
-    import pdb; pdb.set_trace()
-    psi_diff = np.abs(x - x0)
-    ab_x = np.abs(x)
-    ab_x[ab_x < 2*ab_x.eps] = 1
-    x_reldiff = x_diff/ab_x
-    return x_reldiff.max() < tol
+    x_diff = np.abs(x - x0)
+    converged = x_diff.max() < tol
+    return converged
 
 def _var_to_ds(H, Sigma, idx1):
     """Calculate directed spectrum."""
-    import pdb; pdb.set_trace()
-    idx0 = ~idx1
-    H01 = H[:, idx0, idx1]
-    H10 = H[:, idx1, idx0]
-    sig00 = Sigma[idx0, idx0]
-    sig11 = Sigma[idx1, idx1]
-    sig01 = Sigma[idx0, idx1]
-    sig10 = Sigma[idx1, idx0]
+    idx0 = np.nonzero(~idx1)[0]
+    idx1 = np.nonzero(idx1)[0]
+    H01 = H.take(idx0, axis=1).take(idx1, axis=2)
+    H10 = H.take(idx1, axis=1).take(idx0, axis=2)
+    sig00 = Sigma.take(idx0, axis=0).take(idx0, axis=1)
+    sig11 = Sigma.take(idx1, axis=0).take(idx1, axis=1)
+    sig01 = Sigma.take(idx0, axis=0).take(idx1, axis=1)
+    sig10 = Sigma.take(idx1, axis=0).take(idx0, axis=1)
     # conditional covariances
-    sig1_0 = sig11 - solve(sig00.T, sig10.T) @ sig01
-    sig0_1 = sig00 - solve(sig11.T, sig01.T) @ sig10
+    sig1_0 = sig11 - sig10 @ solve(sig00, sig10.conj().T)
+    sig0_1 = sig00 - sig01 @ solve(sig11, sig01.conj().T)
 
-    ds10 = np.real(H01 @ sig1_0 @ H01.H)
-    ds01 = np.real(H10 @ sig0_1 @ H10.H)
+    ds10 = np.real(H01 @ sig1_0 @ H01.conj().transpose(0, 2, 1))
+    ds01 = np.real(H10 @ sig0_1 @ H10.conj().transpose(0, 2, 1))
     return (ds01, ds10)
