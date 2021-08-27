960b1cc3593c2c3105a62e7c84cab4b9175accbb
Neil Gallagher <neil.gallagher@duke.edu>
2021-08-27 09:30:51 -0400

    Acceptable baseline functionality and documentation
    Created a descriptive README
    Updated ds to take 3-D data array, with first dimension being windows
    Removed my hacky testing script (will move to automated testing)
    Updated version number

--- dir_spec/ds.py
+++ dir_spec/ds.py
@@ -20,7 +20,7 @@ class DirectedSpectrum(object):
 
     Attributes
     ----------
-    ds_array : ndarray, shape (n_frequencies, n_groups, n_groups)
+    ds_array : ndarray, shape (n_windows, n_frequencies, n_groups, n_groups)
         Directed spectrum values between each pair of channel
         groups for each frequency.
     f : ndarray, shape (n_frequencies)
@@ -46,9 +46,10 @@ def ds(X, fs, groups, pairwise=False, fres=None, window='hann', nperseg=None,
 
     Parameters
     ----------
-    X : ndarray, shape (n_channels, n_timepoints)
+    X : ndarray, shape (n_windows, n_channels, n_timepoints)
         Timeseries data from multiple channels. It is assumed that the
-        data for each channel are approximately stationary.
+        data for each channel are approximately stationary within a window.
+        If a 2-D array is input, it is assumed that n_windows is 1.
     fs : float
         Sampling rate associated with X.
     groups : list of strings, shape (n_channels)
@@ -87,13 +88,15 @@ def ds(X, fs, groups, pairwise=False, fres=None, window='hann', nperseg=None,
     -------
     dir_spec : DirectedSpectrum object
     """
+    if X.ndim == 2:
+        X = X[np.newaxis]
     cpsd, f = _cpsd_mat(X, fs, fres, window, nperseg, noverlap)
-    # move frequency to first dimension.
-    cpsd = np.moveaxis(cpsd, 2, 0)
+    # move frequency to second dimension.
+    cpsd = np.moveaxis(cpsd, 3, 1)
 
+    ds_array = np.zeros_like(cpsd)
     gidx, grouplist = _group_indicies(groups)
     group_pairs = combinations(range(len(gidx)), 2)
-    ds_array = np.zeros((len(f), len(gidx), len(gidx)))
 
     if not pairwise:
         H, Sigma = _wilson_factorize(cpsd, fs)
@@ -104,22 +107,23 @@ def ds(X, fs, groups, pairwise=False, fres=None, window='hann', nperseg=None,
         pair_idx = np.nonzero(idx0 | idx1)[0]
         sub_idx1 = idx1[pair_idx] # subset of pair_idx in group 1
         if pairwise:
-            sub_cpsd = cpsd.take(pair_idx, axis=1).take(pair_idx, axis=2)
+            sub_cpsd = cpsd.take(pair_idx, axis=-2).take(pair_idx, axis=-1)
             H, Sigma = _wilson_factorize(sub_cpsd, fs)
             ds01, ds10 = _var_to_ds(H, Sigma, sub_idx1)
         else:
-            sub_H = H.take(pair_idx, axis=1).take(pair_idx, axis=2)
-            sub_Sigma = Sigma.take(pair_idx, axis=0).take(pair_idx, axis=1)
+            sub_H = H.take(pair_idx, axis=-2).take(pair_idx, axis=-1)
+            sub_Sigma = Sigma.take(pair_idx, axis=-2).take(pair_idx, axis=-1)
             ds01, ds10 = _var_to_ds(sub_H, sub_Sigma, sub_idx1)
         # average across channels within group.
         # TODO: allow for other options besides average here
-        ds_array[:, gp[0], gp[1]] = np.diagonal(ds01, axis1=1, axis2=2).mean(axis=-1)
-        ds_array[:, gp[1], gp[0]] = np.diagonal(ds10, axis1=1, axis2=2).mean(axis=-1)
+        ds_array[:,:, gp[0], gp[1]] = \
+            np.diagonal(ds01, axis1=-2, axis2=-1).mean(axis=-1)
+        ds_array[:,:, gp[1], gp[0]] = \
+            np.diagonal(ds10, axis1=-2, axis2=-1).mean(axis=-1)
     return DirectedSpectrum(ds_array, f, grouplist)
 
 def _cpsd_mat(X, fs, fres, window, nperseg, noverlap):
     """Return cross power spectral density and associated frequencies."""
-    C, T = X.shape
     # if frequency resolution is set, use it to determine fft length.
     if fres:
         nfft = int(fs/fres)
@@ -128,8 +132,8 @@ def _cpsd_mat(X, fs, fres, window, nperseg, noverlap):
     else:
         nfft = 256
         nperseg = nfft
-    f, cpsd = csd(X, X[:,np.newaxis], fs, window, nperseg, noverlap,
-                  nfft, return_onesided=False, scaling='density')
+    f, cpsd = csd(X[:,:,np.newaxis], X[:,np.newaxis], fs, window, nperseg,
+                  noverlap, nfft, return_onesided=False, scaling='density')
     return (cpsd, f)
 
 def _group_indicies(groups):
@@ -155,43 +159,49 @@ def _wilson_factorize(cpsd, fs, max_iter=1000, tol=1e-9):
     # semidefinite due to rounding errors
     this_eps = np.spacing(np.abs(cpsd)).max()
     U = cholesky(cpsd + np.identity(cpsd.shape[-1])*2*this_eps)
-    for k in range(max_iter):
-        # These lines implement: g = psi \ cpsd / psi* + I
-        psi_inv_cpsd = solve(psi, U)
-        g = psi_inv_cpsd @ psi_inv_cpsd.conj().transpose(0, 2, 1)
-        g = g + np.identity(cpsd.shape[-1])
-        # TODO: compare these update steps to original Wilson algorithm.
-        #       also check if psi[0] should be upper triangular.
-        gplus, g0 = _plus_operator(g)
 
-        # S is chosen so that g0 + S is upper triangular; S + S* = 0
-        S = -np.tril(g0, -1)
-        S = S - S.conj().transpose()
-        gplus = gplus + S
-        psi_prev = psi
-        psi = psi @ gplus
-        A0_prev = A0
-        A0 = A0 @ (g0 + S)
-        if (_check_convergence(psi, psi_prev, tol) and
-                _check_convergence(A0, A0_prev, tol)):
-            break
-        else:
-            warn('Wilson factorization failed to converge.', stacklevel=2)
+    H = np.zeros_like(psi)
+    Sigma = np.zeros_like(A0)
+    for w in range(cpsd.shape[0]):
+        for k in range(max_iter):
+            # These lines implement: g = psi \ cpsd / psi* + I
+            psi_inv_cpsd = solve (psi[w], U[w])
+            g = psi_inv_cpsd @ psi_inv_cpsd.conj().transpose(0, 2, 1)
+            g = g + np.identity(cpsd.shape[-1])
+            # TODO: compare these update steps to original Wilson algorithm.
+            #       also check if psi[0] should be upper triangular.
+            gplus, g0 = _plus_operator(g)
 
-    Sigma = (A0 @ A0.T) * fs
-    H = (solve(A0.T, psi.transpose(0, 2, 1))).transpose(0, 2, 1) # right-side solve
+            # S is chosen so that g0 + S is upper triangular; S + S* = 0
+            S = -np.tril(g0, -1)
+            S = S - S.conj().transpose()
+            gplus = gplus + S
+            psi_prev = psi[w]
+            psi[w] = psi[w] @ gplus
+            A0_prev = A0[w]
+            A0[w] = A0[w] @ (g0 + S)
+            if (_check_convergence(psi[w], psi_prev, tol) and
+                    _check_convergence(A0[w], A0_prev, tol)):
+                break
+            else:
+                warn('Wilson factorization failed to converge.', stacklevel=2)
+
+        # right-side solve
+        H[w] = (solve(A0[w].T, psi[w].transpose(0, 2, 1))).transpose(0, 2, 1) 
+        Sigma[w] = (A0[w] @ A0[w].T) * fs
     return (H, Sigma)
 
 
 def _init_psi(cpsd):
     """Return initial psi value for wilson factorization."""
     # TODO: provide other initialization options; test which is best.
-    gamma = ifft(cpsd, axis=0)
-    gamma0 = np.asmatrix(gamma[0])
+    gamma = ifft(cpsd, axis=1)
+    gamma0 = gamma[:, 0]
+
     # remove assymetry in gamma0 due to rounding error.
-    gamma0 = np.real((gamma0+gamma0.H)/2)
-    h = np.asarray(cholesky(gamma0).H)
-    psi = np.tile(h[np.newaxis], (gamma.shape[0], 1, 1))
+    gamma0 = np.real((gamma0+gamma0.conj().transpose(0, 2, 1))/2)
+    h = cholesky(gamma0).conj().transpose(0, 2, 1)
+    psi = np.tile(h[:, np.newaxis], (1, gamma.shape[1], 1, 1)).astype(complex)
     return psi, h
 
 def _plus_operator(g):
@@ -222,16 +232,16 @@ def _var_to_ds(H, Sigma, idx1):
     idx0 = np.nonzero(~idx1)[0]
     idx1 = np.nonzero(idx1)[0]
 
-    H01 = H.take(idx0, axis=1).take(idx1, axis=2)
-    H10 = H.take(idx1, axis=1).take(idx0, axis=2)
-    sig00 = Sigma.take(idx0, axis=0).take(idx0, axis=1)
-    sig11 = Sigma.take(idx1, axis=0).take(idx1, axis=1)
-    sig01 = Sigma.take(idx0, axis=0).take(idx1, axis=1)
-    sig10 = Sigma.take(idx1, axis=0).take(idx0, axis=1)
+    H01 = H.take(idx0, axis=-2).take(idx1, axis=-1)
+    H10 = H.take(idx1, axis=-2).take(idx0, axis=-1)
+    sig00 = Sigma.take(idx0, axis=-2).take(idx0, axis=-1)
+    sig11 = Sigma.take(idx1, axis=-2).take(idx1, axis=-1)
+    sig01 = Sigma.take(idx0, axis=-2).take(idx1, axis=-1)
+    sig10 = Sigma.take(idx1, axis=-2).take(idx0, axis=-1)
     # conditional covariances
-    sig1_0 = sig11 - sig10 @ solve(sig00, sig10.conj().T)
-    sig0_1 = sig00 - sig01 @ solve(sig11, sig01.conj().T)
+    sig1_0 = sig11 - sig10 @ solve(sig00, sig10.conj().transpose(0, 2, 1))
+    sig0_1 = sig00 - sig01 @ solve(sig11, sig01.conj().transpose(0, 2, 1))
 
-    ds10 = np.real(H01 @ sig1_0 @ H01.conj().transpose(0, 2, 1))
-    ds01 = np.real(H10 @ sig0_1 @ H10.conj().transpose(0, 2, 1))
+    ds10 = np.real(H01 @ sig1_0[:, np.newaxis] @ H01.conj().transpose(0, 1, 3, 2))
+    ds01 = np.real(H10 @ sig0_1[:, np.newaxis] @ H10.conj().transpose(0, 1, 3, 2))
     return (ds01, ds10)
